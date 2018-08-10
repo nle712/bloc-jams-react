12964dc031765f7178d5c5a89463d1bbf553764c
Nicole E <nle712@aol.com>
2018-08-09 22:32:40 -0400

    put album on library page

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ class App extends Component {
         <header>
           <nav>
             <Link to ='/'>Landing</Link>
-            <Link to ='/library'>Library</Link>
-            <Link to = '/library/album'>Album</Link>
+            <Link to ='/library/album'>Library</Link>
           </nav>
           <h1>Bloc Jams</h1>
         </header>
