8cfbd553d540e6685ed8fc234e14475d360191a8
Nicole E <nle712@aol.com>
2018-08-29 22:55:45 -0400

    cleaned up spacing

--- src/components/Album.js
+++ src/components/Album.js
@@ -25,63 +25,64 @@ class Album extends Component {
     this.setState({ isPlaying: true });
   }
 
-   pause() {
-     this.audioElement.pause();
-     this.setState({ isPlaying: false });
-   }
-
-   setSong(song) {
-     this.audioElement.src = song.audioSrc;
-     this.setState({ currentSong: song });
-   }
-
-   handleSongClick(song) {
-     const isSameSong = this.state.currentSong === song;
-     if (this.state.isPlaying && isSameSong) {
-       this.pause();
-     } else {
-       if (!isSameSong) { this.setSong(song); }
-       this.play();
-     }
-   }
-
-   render() {
-     return (
-       <section className="album">
-       <section id="album-info">
-       <img id="album-cover-art"
-       src={this.state.album.albumCover}
-       alt={this.state.album.title}
-       />
-       <div className="album-details">
-       <h1 id="album-title">{this.state.album.title}</h1>
-       <h2 className="artist">{this.state.album.artist}</h2>
-       <div id="release-info">{this.state.album.releaseInfo}</div>
-       </div>
-       </section>
-       <table id ="song-list">
-       <colgroup>
-       <col id="song-number-column" />
-       <col id="song-title-column" />
-       <col id="song-duration-column" />
-       </colgroup>
-       <tbody>
-       {
-         this.state.album.songs.map( (song, index) =>
-
-         <Song key={index}
-         data={song}
-         number={index + 1}
-         handleClick={this.handleSongClick.bind(this)}
-         isPlaying={song === this.state.currentSong && this.state.isPlaying}/>
-       )
-     }
-     </tbody>
-     </table>
-     </section>
-   );
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
   }
- }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
+  }
+
+  render() {
+    return (
+      <section className="album">
+        <section id="album-info">
+          <img id="album-cover-art"
+            src={this.state.album.albumCover}
+            alt={this.state.album.title}
+          />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id ="song-list">
+          <colgroup>
+            <col id="song-number-column" />
+            <col id="song-title-column" />
+            <col id="song-duration-column" />
+          </colgroup>
+          <tbody>
+            {
+              this.state.album.songs.map( (song, index) =>
+                <Song
+                  key={index}
+                  data={song}
+                  number={index + 1}
+                  handleClick={this.handleSongClick.bind(this)}
+                  isPlaying={song === this.state.currentSong && this.state.isPlaying}
+                />
+              )
+            }
+          </tbody>
+        </table>
+      </section>
+    );
+  }
+}
 
 
 export default Album;
