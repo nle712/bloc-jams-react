11a590d3d8eb084ed4dea117bbd1d9fca5a3cf43
Nicole E <nle712@aol.com>
2018-09-04 23:00:17 -0400

    icons rendering

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -6,10 +6,10 @@ class PlayerBar extends Component {
       <section className="player-bar">
         <section id="buttons">
           <button id="previous" onClick={this.props.handlePrevClick}>
-            <span className="ion-skip-backward"></span>
+            <span className="icon ion-md-skip-backward"></span>
           </button>
           <button id="play-pause" onClick={this.props.handleSongClick} >
-            <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
+            <span className={this.props.isPlaying ? 'icon ion-md-pause' : 'icon ion-md-play'}></span>
           </button>
           <button id="next">
             <span className="ion-skip-forward"></span>
@@ -17,12 +17,12 @@ class PlayerBar extends Component {
         </section>
         <section id="time-control">
           <div className="current-time">–:––</div>
-          <input type="range" className="seek-bar" value="0" />
+          <input type="range" className="seek-bar" defaultValue="0" />
           <div className="total-time">–:––</div>
         </section>
         <section id="volume-control">
           <div className="icon ion-volume-low"></div>
-          <input type="range" className="seek-bar" value="80" />
+          <input type="range" className="seek-bar" defaultValue="80" />
           <div className="icon ion-volume-high"></div>
         </section>
       </section>
