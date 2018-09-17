123b90bb43d7793f24eda7c2f8cfbf4bfffa761f
Nicole E <nle712@aol.com>
2018-09-16 23:32:24 -0400

    fixed duration time

--- src/components/Album.js
+++ src/components/Album.js
@@ -111,9 +111,9 @@ class Album extends Component {
             src={this.state.album.albumCover}
             alt={this.state.album.title}
           />
-          <div className="album-details">
-            <h1 id="album-title">{this.state.album.title}</h1>
-            <h2 className="artist">{this.state.album.artist}</h2>
+          <div className="album-details text-danger">
+            <h1 id="album-title ">{this.state.album.title}</h1>
+            <h2 className="artist text-danger">{this.state.album.artist}</h2>
             <div id="release-info">{this.state.album.releaseInfo}</div>
           </div>
         </section>
